9ea4ab4063538a1d80d8e713159de68539e41d24
Vivianne <viviannevilar@gmail.com>
2020-06-29 10:04:46 +0800

    added lists Q3 and Q4

---
+++ listsQ3.py
@@ -0,0 +1,13 @@
+
+
+
+names = [None,None,None]
+position = ["first","second","third"]
+
+
+for i in range(3):
+    names[i] = input(f"What's the {position[i]} name: ")
+
+print()
+print(names)
+print()
---
+++ listsQ4.py
@@ -0,0 +1,11 @@
+
+
+
+my_string = input("Enter a string: ")
+
+words = my_string.split()
+
+letters = list(my_string)
+
+print(f"{len(words)} {words}")
+print(f"{len(letters)} {letters}")
