a772b6125ad27383821d90e4f5953cad599eb295
Vivianne <viviannevilar@gmail.com>
2020-06-29 09:54:56 +0800

    lists Q1 and Q2

---
+++ listsQ1.py
@@ -0,0 +1,20 @@
+foods = [
+"orange",
+"apple",
+"banana",
+"strawberry",
+"grape",
+"blueberry",
+["carrot", "cauliflower", "pumpkin"], "passionfruit",
+"mango",
+"kiwifruit"
+]
+
+print()
+print(f"The first item in the list is {foods[0]}")
+print(f"The third item in the list is {foods[2]}")
+print(f"The last item in the list is {foods[-1]}")
+print(f"The first three items in the list are {foods[0:3]}")
+print(f"The last three items in the list are {foods[-3:]}")
+print(f"The last item in the sublist is {foods[6][-1]}")
+print()
---
+++ listsQ2.py
@@ -0,0 +1,14 @@
+mailing_list = [
+["Roary", "roary@moth.catchers"],
+["Remus", "remus@kapers.dog"],
+["Prince Thomas of Whitepaw", "hrh.thomas@royalty.wp"], ["Biscuit", "biscuit@whippies.park"],
+["Rory", "rory@whippies.park"],
+]
+
+print()
+
+dim = len(mailing_list)
+for i in range(dim):
+    print(f"{mailing_list[i][0]}: {mailing_list[i][1]}")
+
+print()
