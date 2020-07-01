9dcced9d35b8a6a40299958a86e1f589588e7f3b
Vivianne <viviannevilar@gmail.com>
2020-07-01 18:56:28 +0800

    Updated filesQ2.py

--- filesQ2.py
+++ filesQ2.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-my_data = pd.read_csv("students/exercise_data/colours_20.csv")
-#data_answer = my_data[[' RGB',' HEX',' English']]
-data_answer = my_data.iloc[:,[1,2,4]]
-print(data_answer.to_string(index=False))
+files = {"colours_20","colours_213"}
 
-print()
-my_data = pd.read_csv("students/exercise_data/colours_213.csv")
-data_answer = my_data.iloc[:,[1,2,4]]
-print(data_answer.to_string(index=False))
+for x in files:
+    my_data = pd.read_csv(f"students/exercise_data/{x}.csv")
+    data_answer = my_data.iloc[:,[1,2,4]]
+    print(data_answer.to_string(index=False))
+    print()
+
+#data_answer = my_data[[' RGB',' HEX',' English']]
